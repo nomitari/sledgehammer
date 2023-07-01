1c63097c0e9c22c0e10247bda75cf930924467ea
Maya Bennett <mayabennett@Mayas-MacBook-Pro.local>
2023-06-30 21:44:22 -0500

    components and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Home />
+      <Services />
+      <Portfolio />
+      <About aboutText="Hello! I'm a young female entrepreneur with a passion for helping businesses grow in the digital age. With my skills as a full-stack developer and my keen eye for market trends, I provide businesses with the tools they need to succeed." />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
